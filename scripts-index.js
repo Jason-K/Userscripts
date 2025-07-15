e0aa0a7cdae56599d0100d99c29eef1a2e45fcdf
Jason <80914358+Jason-K@users.noreply.github.com>
2025-07-15 14:05:38 -0700

    Remove scripts-index.js and clean up comments in scripts-index.json

--- scripts-index.js
+++
@@ -1,7 +0,0 @@
-// scripts-index.js
-window.USERSCRIPTS = [
-  "consensus_copy-citations.user.js",
-  "wikipedia_copy-citations.user.js",
-  "merus_tab-send4spaces.user.js",
-  "pdrater_copy-rating.user.js",
-];
